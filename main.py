aa208a4b96a7bcf065c02a2778b2258d197a50ac
iosif <ion.popescu@gmail.com>
2023-05-15 17:50:25 +0300

    final

--- main.py
+++ main.py
@@ -1,75 +1,44 @@
-import threading
-import multiprocessing
-from concurrent.futures import ThreadPoolExecutor
-import time
 import random
+from threading import Thread, Condition
+import time
+from queue import Queue
 
-vect = []
-dim = 1000
-for i in range(0, dim):
-    vect.append(random.randint(0,dim))
-
-
-def sortare(index1, index2, v):
-    cv = []
-    for i in range(index1, index2):
-        cv.append(v[i])
-    cv.sort()
-    print(cv)
+coada = Queue()
 
 
-def ver_1():
-    ind = int(dim/2)
-    thread_1 = threading.Thread(target=sortare(0,ind,vect))
-    thread_2 = threading.Thread(target=sortare(ind+1, dim, vect))
-    thread_1.start()
-    thread_2.start()
-    rez1 = thread_1.join()
-    rez2 = thread_2.join()
+class Consumator(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+
+    def consumator(self):
+        x = coada.get()
+        print("Am consumat nr: "+str(x)+"\n")
+
+    def run(self):
+        while(True):
+            self.consumator()
 
 
+class Producator(Thread):
+    def __init__(self):
+        Thread.__init__(self)
 
-def ver_2():
-    ind = int(dim / 2)
-    sortare(0, ind, vect)
-    sortare(ind, dim, vect)
+    def producator(self):
+        x = random.randint(0,100)
+        print("Pun "+str(x)+"in coada\n")
+        coada.put(x)
+        #time.sleep(1)
 
-
-def ver_3():
-    ind = int(dim / 2)
-    process_1 = multiprocessing.Process(target=sortare(0,ind,vect))
-    process_2 = multiprocessing.Process(target=sortare(ind+1, dim, vect))
-    process_1.start()
-    process_2.start()
-    process_1.join()
-    process_2.join()
-
-
-def ver_4():
-    ind = int(dim / 2)
-    with ThreadPoolExecutor(max_workers=2) as executor:
-        future = executor.submit(sortare(0, ind, vect))
-        future = executor.submit(sortare(ind+1, dim, vect))
+    def run(self):
+        while(True):
+            self.producator()
 
 
 if __name__ == '__main__':
-    start = time.time()
-    ver_1()
-    end = time.time()
-    print("\n Timp executie pseudoparalelism cu GIL")
-    print(end - start)
-    start = time.time()
-    ver_2()
-    end = time.time()
-    print("\n Timp executie secvential")
-    print(end - start)
-    start = time.time()
-    ver_3()
-    end = time.time()
-    print("\n Timp executie paralela cu multiprocessing")
-    print(end - start)
-    start = time.time()
-    ver_4()
-    end = time.time()
-    print("\n Timp executie paralela cu concurrent.futures")
-    print(end - start)
+    producator = Producator()
+    consumator = Consumator()
+    consumator.start()
+    producator.start()
+
+    consumator.join()
+    producator.join()
